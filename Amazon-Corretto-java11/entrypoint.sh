56c9f36e2d64132a5f577cbb3c464f3021690065
MG8853 <qq3082499405@icloud.com>
2021-09-20 19:17:40 +0900

    i

--- Amazon-Corretto-java11/entrypoint.sh
+++ Amazon-Corretto-java11/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/ash
 cd /home/container
 
+# Output Current Java Version
+java -version
+
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
