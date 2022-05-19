05becde3198f8fe3872dc1d4de5bc3b7389bc791
MG8853 <qq3082499405@icloud.com>
2022-05-19 11:43:42 +0900

    add DiscordTTSBotVOICEVOX.yml

---
+++ DiscordTTSBotVOICEVOX/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sleep 1
+
+cd /home/container
+
+node -v
+npm -v
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
---
+++ DiscordTTSBotVOICEVOX/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Setup BotToken..."
+export BOT_TOKEN="<Discord Developer Portalで取得したトークン>"
+
+echo "Starting DiscordBot..."
+npm start
+
