2da9eb6d30eb439cb7872b093fd54673ecdc4aa9
MG8853 <qq3082499405@icloud.com>
2022-06-13 10:27:42 +0900

    ubuntu 20

--- ubuntu-20.04/entrypoint.sh
+++ ubuntu-20.04/entrypoint.sh
@@ -4,9 +4,17 @@ sleep 1
 
 cd /home/container
 
+# Default the TZ environment variable to Tokyo.
+TZ=Asia/Tokyo
+export TZ
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
