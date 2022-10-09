d7af388fdb9e4013af0ad2e2368dc54628e77587
MultiGamer8853 <qq3082499405@icloud.com>
2022-10-09 12:46:56 +0900

    Update entrypoint.sh

--- VOICEVOX_engine/entrypoint.sh
+++ VOICEVOX_engine/entrypoint.sh
@@ -1,21 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-sleep 1
+cd /home/container
 
+# Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Switch to the container's working directory
-cd /home/container || exit 1
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-python --version
-
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
-
-# shellcheck disable=SC2086
-eval ${PARSED}
-
+# Run the Server
+eval ${MODIFIED_STARTUP}
