49816e0ebc9b7247c537309e2d36db196f452ddf
MG8853 <qq3082499405@icloud.com>
2024-04-14 22:45:17 +0900

    voicevox用テストno3

--- VOICEVOX_engine/entrypoint.sh
+++ VOICEVOX_engine/entrypoint.sh
@@ -6,6 +6,9 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
+# Display README for engine
+cat /opt/voicevox_engine/README.md > /dev/stderr
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
